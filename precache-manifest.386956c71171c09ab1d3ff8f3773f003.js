3cd4aeb1f6b698092208b71c10daa757f973f746
TevinHlungwani <xigazatainment@gmail.com>
2020-09-17 00:22:34 +0200

    ;

---
+++ precache-manifest.386956c71171c09ab1d3ff8f3773f003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1c3ba9ec9411ce6ed8d19087cce3ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1366aa87d71e0ca2be96",
+    "url": "/static/css/main.c70e20a8.chunk.css"
+  },
+  {
+    "revision": "45d024fa12e0b5480902",
+    "url": "/static/js/2.d3a6aefb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3a6aefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1366aa87d71e0ca2be96",
+    "url": "/static/js/main.28f6ce21.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  }
+]);
